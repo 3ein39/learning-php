2fa102b55015bc02a5047f4f81ff3e570a649e30
Hussein Hany <hussein.hany.cs@gmail.com>
2024-07-11 19:25:34 +0300

    Learn output lesson

---
+++ 01_output.php
@@ -0,0 +1,18 @@
+<?php
+// echo strings, numbers, variables, html etc.
+echo "hello world\n", "testing\n\n";
+
+// print for single lines
+print "hello world\n";
+
+// print_r for arrays
+print_r([0, 1, 2]);
+
+// var_dump for debugging, shows type and length
+var_dump([0, 1, 2]);
+var_dump("hussein");
+
+// var_export string representation of a variable
+var_export("hello");
+
+?>
